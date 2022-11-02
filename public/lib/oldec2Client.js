637687de1ddaeb45016f3d9bdea5530098f7f30a
Erdinc Kaya <erdincka@msn.com>
2022-11-02 02:05:40 +0000

    clean up

--- public/lib/oldec2Client.js
+++
@@ -1,4 +0,0 @@
-const  { EC2Client } = require( "@aws-sdk/client-ec2");
-// Create anAmazon EC2 service client object.
-const ec2Client = (REGION) => new EC2Client({ region: REGION });
-module.exports = { ec2Client };
--- src/lib/Wizard/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./StepContent";
-export * from "./StepFooter";
-export * from "./StepHeader";
-export * from "./WizardContext";
-export * from "./WizardHeader";
